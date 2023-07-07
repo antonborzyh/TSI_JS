4d7cd883c81fb251a8b431b48ddadb3d70c8cbb4
aborzykh <ayborzykh@gmail.com>
2023-07-07 08:37:19 +0200

    Add main SpringBoot class. Add application.properties.

---
+++ src/main/java/com/tsieducation/TrainTicketsApplication.java
@@ -0,0 +1,14 @@
+package com.tsieducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TrainTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainTicketsApplication.class, args);
+    }
+
+}
